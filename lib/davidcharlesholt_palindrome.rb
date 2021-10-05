cd8aebc19204436c64e0a4e7b7e719d8ea751364
davidcharlesholt <davidcharlesholt@gmail.com>
2021-10-05 15:33:44 -0700

    Finish working and refactoring palindrome method

--- lib/davidcharlesholt_palindrome.rb
+++ lib/davidcharlesholt_palindrome.rb
@@ -2,7 +2,17 @@
 
 require_relative "davidcharlesholt_palindrome/version"
 
-module DavidcharlesholtPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
