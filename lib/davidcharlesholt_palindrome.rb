19518bc84ebb89fb042b3c7b15e9283bf2193c0e
davidcharlesholt <davidcharlesholt@gmail.com>
2021-10-05 14:08:20 -0700

    Initialize repository

---
+++ lib/davidcharlesholt_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "davidcharlesholt_palindrome/version"
+
+module DavidcharlesholtPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
